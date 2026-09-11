ee68b11b06fffc4c4e8ebd24a461d7d72766197d
JenX <jenny.delumen@gmail.com>
2026-09-11 19:42:26 +0800

    Add web app service worker

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE='games-of-alia-v1';
+const CORE=[
+  '/games_of_Alia/',
+  '/games_of_Alia/index.html',
+  '/games_of_Alia/home.css',
+  '/games_of_Alia/builder-quest/',
+  '/games_of_Alia/style.css',
+  '/games_of_Alia/config.js',
+  '/games_of_Alia/questions.js',
+  '/games_of_Alia/app.js',
+  '/games_of_Alia/icons/games-of-alia-180.png'
+];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
